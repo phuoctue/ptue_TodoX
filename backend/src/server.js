5a4a9956e651a425ee00b6e16746be3ddad6317b
phuoctue <phuoctue0312@gmail.com>
2026-04-21 00:01:46 +0700

    chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,31 @@ import dotenv from "dotenv";
 import taskRoute from "./routes/taskRouters.js";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
-
+import path, { dirname } from "path";
 dotenv.config({ path: "./src/.env" });
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 //middlewares
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 // routes
 app.use("/api/tasks", taskRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 // connect database
 connectDB().then(() => {
